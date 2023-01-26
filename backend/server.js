ce0660bdde88bc236f496ce3055481284c43606b
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-01-26 15:55:38 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ function sendMail(email, subject, message) {
 // app.use('/userupdate', cors());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://notaroomba.xyz");
+  res.header("Access-Control-Allow-Origin", "notaroomba.xyz");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
