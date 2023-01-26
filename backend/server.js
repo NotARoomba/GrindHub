753020832d107aece757f6b2e8350f8dd324c5af
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-01-26 11:04:10 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,20 @@ async function main () {
   })
 const app = express()
 
-app.use(cors())
+
+const allowedOrigins = ['http://localhost:3000', 'https://grindhub.notaroomba.xyz'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 app.use(bodyParser.json())
 
 
