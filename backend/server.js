f6440858112223d1f5270c1f7f7e7537c9d0aaf9
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-01-27 10:04:13 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ const allowedOrigins = ['http://localhost:3000', 'https://grindhub.notaroomba.xy
 app.use(cors({
   origin: allowedOrigins,
   preflightContinue: true,
+  credentials: true
 }));
 app.use(bodyParser.json())
 
