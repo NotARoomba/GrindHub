7aa923791b63f776f4443095bad37f0cabbfec34
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-01-28 12:58:47 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,20 +10,24 @@ const express = require('express')
 const cors = require('cors');
 const { error } = require('console');
 var winston = require('winston');
+const os = require('os');
+require('winston-syslog');
+
+const papertrail = new winston.transports.Syslog({
+  host: 'logs2.papertrailapp.com',
+  port: 53939,
+  protocol: 'tls4',
+  localhost: os.hostname(),
+  eol: '\n',
+});
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
+  format: winston.format.simple(),
+  levels: winston.config.syslog.levels,
+  transports: [papertrail],
 });
+logger.info('INIT APP')
+
 function main () {
   MongoClient.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true, keepAlive: true }).then(mongo => {
 
