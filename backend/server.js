b80e976519949949f1218785519612803431b889
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-01-30 08:17:21 -0500

    f

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,9 @@ async function main () {
   const mongo = await MongoClient.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true, keepAlive: true })
 const app = express()
 
+let bot = new chatGPT(process.env.OPENAI_TOKEN);
+logger.info("CHATGPT INIT")
+await bot.waitForReady().then(() => logger.info("CHATGPT READY")).catch(err => logger.info(err))
 const allowedOrigins = ['http://localhost:3000', 'https://grindhub.notaroomba.xyz', 'https://notaroomba.xyz', 'http://grindhub.notaroomba.xyz'];
 
 app.use(cors({
@@ -97,10 +100,7 @@ const users = mongo.db("userData").collection("users");
     res.send(0)
 })
 app.get("/getmissions", async (req, res) => {
-let bot = new chatGPT(process.env.OPENAI_TOKEN);
-logger.info("CHATGPT INIT")
-await bot.waitForReady().then(() => logger.info("CHATGPT READY")).catch(err => logger.info(err))
-  let response = await bot.ask("write this data into a json object: write 12 missions about daily habits or wellbeing and categorize them into categories made up of defense, intelligence and strength. They should be in 2 groups of 6 missions divided into 3 groups of 2, also write a stat for each of them upgrading their parent category by a random number under 20. Make a description for each of the missions then write all the values into a json object using only the values: name, description, category, upgrade").catch(err => logger.info(err))
+  let response = await bot.ask("write this data into a json object: write 12 missions about daily habits or wellbeing and categorize them into categories made up of defense, intelligence and strength. They should be in 2 groups of 6 missions divided into 3 groups of 2, also write a stat for each of them upgrading their parent category by a random number under 20. Make a description for each of the missions then write all the values into a json object using only the values: name, description, category, upgrade. make the keys lowercase").catch(err => logger.info(err))
   logger.info(response)
   res.send(response)
 })
