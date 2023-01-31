0dfd3ea96fe31684b2e2cd3636de603320f11b68
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-01-31 16:45:53 -0500

    asdf

--- index.js
+++ index.js
@@ -142,6 +142,7 @@ async function updateMissions(beenClicked) {
   if (beenClicked) await superagent.post(BACKEND_URL + "/userupdate").send(({ key: getCookie('userKey') }, { $set: { hasRefreshed: true } }))
   let missions = await superagent.post(BACKEND_URL + "/missions").send(({ time: { $gt: (Date.now() - 86400000) } }, { sort: { time: -1 }, }))
   if (missions.body.missions.length == 0) missions = await superagent.post(BACKEND_URL + "/missions").send(({ time: { $lt: (Date.now() - 86400000) } }, { sort: { time: -1 }, }))
+  console.log(missions)
   missions = missions.body.missions
   let user = await superagent.post(BACKEND_URL + "/user").send({ key: getCookie('userKey') })
   if (user.body.hasRefreshed == null) {
