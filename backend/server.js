5ec36eceecb36dca0d890673003ddf7f27ca42b3
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-01-31 07:37:09 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -110,8 +110,8 @@ app.get("/getmissions", async (req, res) => {
     max_tokens: 3500,
     prompt: 'write this data into a json object: write 12 missions about daily habits or wellbeing and categorize them into categories made up of defense, intelligence and strength. They should be in 2 groups of 6 missions divided into 3 groups of 2, also write a stat for each of them upgrading their parent category by a random number under 20. Make a description for each of the missions then write all the values into a json object using only the values: name, description, category, upgrade. make the keys lowercase, an example of this would be ``` { missions: [{name: "Drink Water", description: "Drink some water for your health", category: "defense", upgrade: 12] ,...}``` make sure that the array of missions has 12 objects',
   }).catch(err => logger.info(err))
-  logger.info(completion)
-  res.send(completion.data.choices[0])
+  logger.info(JSON.parse(completion.data.choices[0].text.replace(/\n/g, "\\n").replace(/\r/g, "\\r").replace(/\t/g, "\\t")))
+  res.json(JSON.parse(completion.data.choices[0].text.replace(/\n/g, "\\n").replace(/\r/g, "\\r").replace(/\t/g, "\\t")))
 })
   
 // start the server
