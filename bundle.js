43d6c1e93765a8589cbe80d95744cae914fea983
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-01-30 19:44:04 -0500

    time

--- bundle.js
+++ bundle.js
@@ -6,7 +6,7 @@ const BACKEND_URL = "https://grindhub-backend.onrender.com"
 
 function setCookie(key, value) {
   var expires = new Date();
-  expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+  expires.setTime(expires.getTime() + (10 * 365 * 24 * 60 * 60 * 1000));
   document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
 }
 
