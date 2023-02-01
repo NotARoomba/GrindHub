8f544a9cda2eb377921b271b017bc338bc2f5405
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-02-01 13:53:11 -0500

    8 hours whoops

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ async function logout() {
 async function getMissions() {
   //get mission
   let last = await superagent.post(BACKEND_URL + "/missions").send([{ time: { $gt: 0 } }, { sort: { time: -1 }, }])
-  if ((last.body.length == 0 && last.text === "[]") || (last.body[0].time <= (Date.now() - 864000))) {
+  if ((last.body.length == 0 && last.text === "[]") || (last.body[0].time <= (Date.now() - 86400))) {
     //in json format, write 6 missions about daily habits or wellbeing and categorize them into categories made up of defense, intelligence and strength. They should be in 3 groups of 2 divided equally, then write stats for each of them upgrading their parent category by a number under 20 and another category that is similar upgraded with a number that is under 10
     const data = await superagent.get(BACKEND_URL + "/getmissions")
     console.log("GETTING MISSIONS")
